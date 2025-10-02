25e0605e4838af95908887d40b3e9d96105112be
gabrielalimact <lgcl@ic.ufal.br>
2025-10-02 07:43:13 -0300

    refactor: remover comentário

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001', 'https://escritorio-dnascimento.cloud'], // ajuste para a porta do seu frontend
+    origin: ['http://localhost:3001', 'https://escritorio-dnascimento.cloud'],
     credentials: true,
   });
   const config = new DocumentBuilder()
