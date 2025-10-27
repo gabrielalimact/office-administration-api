416b1ac1df1266b4a878068ff58bcc6451f3bd30
gabrielalimact <lgcl@ic.ufal.br>
2025-10-27 08:42:40 -0300

    feat: adicionar suporte para servir arquivos estáticos da pasta imagens

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Servir arquivos estáticos da pasta imagens
+  app.useStaticAssets(join(__dirname, '..', 'imagens'), {
+    prefix: '/imagens/',
+  });
 
   app.enableCors({
     origin: [
