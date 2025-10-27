5247217ba79f9abbabba8ba230b7ea2b1302ce0c
gabrielalimact <lgcl@ic.ufal.br>
2025-10-27 08:43:47 -0300

    feat: remover suporte ao Multer e simplificar o módulo Arquivo

--- src/modules/arquivo/arquivo.module.ts
+++ src/modules/arquivo/arquivo.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MulterModule } from '@nestjs/platform-express';
 import { ArquivoController } from './arquivo.controller';
 import { ArquivoService } from './arquivo.service';
 import { Arquivo } from './entities/arquivo.entity';
-import { MulterConfigService } from './multer-config.service';
-
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Arquivo]),
-    MulterModule.registerAsync({
-      useClass: MulterConfigService,
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([Arquivo])],
   controllers: [ArquivoController],
-  providers: [ArquivoService, MulterConfigService],
+  providers: [ArquivoService],
   exports: [ArquivoService],
 })
 export class ArquivoModule {}
