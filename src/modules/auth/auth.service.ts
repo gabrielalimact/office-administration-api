e24b7a6ffa749d6dc5db7133e74be5bd6f928187
gabrielalimact <lgcl@ic.ufal.br>
2025-11-17 11:26:28 -0300

    fix: alterar tempo de expiração do refresh token de 7d para 1d

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -37,7 +37,7 @@ export class AuthService {
 
     const refreshToken = this.jwtService.sign(payload, {
       secret: process.env.JWT_REFRESH_SECRET,
-      expiresIn: '7d',
+      expiresIn: '1d',
     });
 
     return {
